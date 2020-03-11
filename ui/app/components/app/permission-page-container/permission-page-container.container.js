28de4d3e005be3fbee098aff86c2f8f5c16a8a9e
Dan Miller <danjm.com@gmail.com>
2020-03-11 12:18:02 -0230

    Implement tooltip and styled sentence parts on header text of second screen of multi-account-select flow

--- ui/app/components/app/permission-page-container/permission-page-container.container.js
+++ ui/app/components/app/permission-page-container/permission-page-container.container.js
@@ -3,20 +3,25 @@ import PermissionPageContainer from './permission-page-container.component'
 import {
   getPermissionsDescriptions,
   getDomainMetadata,
+  getMetaMaskIdentities,
 } from '../../../selectors/selectors'
 
 const mapStateToProps = (state, ownProps) => {
-  const { request, cachedOrigin } = ownProps
+  const { request, cachedOrigin, selectedIdentities } = ownProps
   const { metadata: requestMetadata = {} } = request || {}
 
   const domainMetadata = getDomainMetadata(state)
   const origin = requestMetadata.origin || cachedOrigin
   const targetDomainMetadata = (domainMetadata[origin] || { name: origin, icon: null })
 
+  const allIdentities = getMetaMaskIdentities(state)
+  const allIdentitiesSelected = Object.keys(selectedIdentities).length === Object.keys(allIdentities).length
+
   return {
     permissionsDescriptions: getPermissionsDescriptions(state),
     requestMetadata,
     targetDomainMetadata,
+    allIdentitiesSelected,
   }
 }
 
